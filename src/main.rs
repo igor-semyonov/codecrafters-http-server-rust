f07b765e97d64770fc60ca87e73feac7537e8e39
Igor Semyonov <igor@semyonov.xyz>
2024-07-09 14:52:03 -0400

    should be working

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+    let listener =
+        TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    let response = b"HTTP/1.1 200 OK\\r\\n\\r\\n";
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut open_stream) => {
+                println!(
+                    "accepted new connection from {}",
+                    open_stream.peer_addr()?
+                );
+                open_stream
+                    .write(response)?;
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!(
+                    "error: {}",
+                    e
+                );
             }
         }
     }
+
+    Ok(())
 }
