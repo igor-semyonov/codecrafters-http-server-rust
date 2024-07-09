a04238c20a233804421d012d805b2ea7559890b2
Igor Semyonov <igor@semyonov.xyz>
2024-07-09 17:24:10 -0400

    aded 404

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() -> std::io::Result<()> {
     println!("Logs from your program will appear here!");
@@ -6,7 +9,6 @@ fn main() -> std::io::Result<()> {
     let listener =
         TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    let response = b"HTTP/1.1 200 OK\r\n\r\n";
     for stream in listener.incoming() {
         match stream {
             Ok(mut open_stream) => {
@@ -14,8 +16,18 @@ fn main() -> std::io::Result<()> {
                     "accepted new connection from {}",
                     open_stream.peer_addr()?
                 );
+                let mut request_buffer = [0_u8; 1024];
                 open_stream
-                    .write_all(response)?;
+                    .read(&mut request_buffer)?;
+                let response = if request_buffer
+                    .starts_with(b"GET / HTTP")
+                {
+                    "HTTP/1.1 200 OK\r\n\r\n"
+                } else {
+                    "HTTP/1.1 404 Not Found\r\n\r\n"
+                };
+                open_stream
+                    .write_all(response.as_bytes())?;
             }
             Err(e) => {
                 println!(
