d97f258ae0ec178c2ab517431a3ee94ca66d3dc1
Igor Semyonov <igor@semyonov.xyz>
2024-07-11 00:57:42 -0400

    added threading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
 
 mod http;
@@ -14,106 +14,19 @@ fn main() -> std::io::Result<()> {
     let listener =
         TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    'stream: for stream in listener.incoming() {
+    for stream in listener.incoming() {
         match stream {
             Ok(mut open_stream) => {
                 println!(
                     "accepted new connection from {}",
                     open_stream.peer_addr()?
                 );
-                let mut request_buffer =
-                    [0_u8; REQUEST_BUFFEX_SIZE];
-                let request_buffer_len = open_stream
-                    .read(&mut request_buffer)?;
-                if request_buffer_len > REQUEST_BUFFEX_SIZE
-                {
-                    // received request that exceeds buffer
-                    // length
-                    continue 'stream;
-                }
-                let request_buffer =
-                    &request_buffer[0..request_buffer_len];
-                let request: Request =
-                    request_buffer.into();
-
-                let response = if request.method
-                    == HttpMethod::Get
-                    && request.target == "/"
-                {
-                    Response {
-                        version: HttpVersion::V1_1,
-                        code: ResponseCode::C200,
-                        headers:
-                            std::collections::HashMap::new(),
-                        body: "".to_string(),
-                    }
-                } else if request.method == HttpMethod::Get
-                    && request
-                        .target
-                        .starts_with("/user-agent")
-                {
-                    let body = request
-                        .headers
-                        .get("User-Agent")
-                        .unwrap()
-                        .to_string();
-                    let mut headers =
-                        std::collections::HashMap::new();
-                    headers.insert(
-                        "Content-Length".to_string(),
-                        body.as_bytes()
-                            .len()
-                            .to_string(),
-                    );
-                    headers.insert(
-                        "Content-Type".to_string(),
-                        "text/plain".to_string(),
-                    );
-                    Response {
-                        version: HttpVersion::V1_1,
-                        code: ResponseCode::C200,
-                        headers,
-                        body,
-                    }
-                } else if request.method == HttpMethod::Get
-                    && request
-                        .target
-                        .starts_with("/echo/")
-                {
-                    let body = request
-                        .target
-                        .trim_start_matches("/echo/")
-                        .to_string();
-                    let mut headers =
-                        std::collections::HashMap::new();
-                    headers.insert(
-                        "Content-Length".to_string(),
-                        body.as_bytes()
-                            .len()
-                            .to_string(),
-                    );
-                    headers.insert(
-                        "Content-Type".to_string(),
-                        "text/plain".to_string(),
-                    );
-                    Response {
-                        version: HttpVersion::V1_1,
-                        code: ResponseCode::C200,
-                        headers,
-                        body,
-                    }
-                } else {
-                    let headers = std::collections::HashMap::new();
-                    Response {
-                        version: HttpVersion::V1_1,
-                        code: ResponseCode::C404,
-                        headers,
-                        body: "".to_string(),
-                    }
-                };
-                let s: String = response.into();
-                open_stream
-                    .write_all(s.as_bytes())?;
+                // handle_connection(&mut open_stream);
+                std::thread::spawn(
+                    move || {
+                        handle_connection(&mut open_stream)
+                    },
+                );
             }
             Err(e) => {
                 println!(
@@ -123,6 +36,98 @@ fn main() -> std::io::Result<()> {
             }
         }
     }
+
+    Ok(())
+}
+
+fn handle_connection(
+    open_stream: &mut TcpStream,
+) -> std::io::Result<()> {
+    let mut request_buffer = [0_u8; REQUEST_BUFFEX_SIZE];
+    let request_buffer_len =
+        open_stream.read(&mut request_buffer)?;
+    if request_buffer_len > REQUEST_BUFFEX_SIZE {
+        // received request that exceeds buffer
+        // length
+        return Ok(());
+    }
+    let request_buffer =
+        &request_buffer[0..request_buffer_len];
+    let request: Request = request_buffer.into();
+
+    let response = if request.method == HttpMethod::Get
+        && request.target == "/"
+    {
+        Response {
+            version: HttpVersion::V1_1,
+            code: ResponseCode::C200,
+            headers: std::collections::HashMap::new(),
+            body: "".to_string(),
+        }
+    } else if request.method == HttpMethod::Get
+        && request
+            .target
+            .starts_with("/user-agent")
+    {
+        let body = request
+            .headers
+            .get("User-Agent")
+            .unwrap()
+            .to_string();
+        let mut headers = std::collections::HashMap::new();
+        headers.insert(
+            "Content-Length".to_string(),
+            body.as_bytes()
+                .len()
+                .to_string(),
+        );
+        headers.insert(
+            "Content-Type".to_string(),
+            "text/plain".to_string(),
+        );
+        Response {
+            version: HttpVersion::V1_1,
+            code: ResponseCode::C200,
+            headers,
+            body,
+        }
+    } else if request.method == HttpMethod::Get
+        && request
+            .target
+            .starts_with("/echo/")
+    {
+        let body = request
+            .target
+            .trim_start_matches("/echo/")
+            .to_string();
+        let mut headers = std::collections::HashMap::new();
+        headers.insert(
+            "Content-Length".to_string(),
+            body.as_bytes()
+                .len()
+                .to_string(),
+        );
+        headers.insert(
+            "Content-Type".to_string(),
+            "text/plain".to_string(),
+        );
+        Response {
+            version: HttpVersion::V1_1,
+            code: ResponseCode::C200,
+            headers,
+            body,
+        }
+    } else {
+        let headers = std::collections::HashMap::new();
+        Response {
+            version: HttpVersion::V1_1,
+            code: ResponseCode::C404,
+            headers,
+            body: "".to_string(),
+        }
+    };
+    let s: String = response.into();
+    open_stream.write_all(s.as_bytes())?;
 
     Ok(())
 }
